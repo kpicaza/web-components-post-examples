0ffef753808ff15717656c4bf452155ca6aaac52
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2022-09-30 17:43:55 +0200

    Create native web component

---
+++ native/components/word-count-paragraph.js
@@ -0,0 +1,48 @@
+export default class WordCountParagraph extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const styles = `
+            .wrapper {
+              border: solid 1px #ccc;
+              display: block;
+              padding: 5px 25px;
+            }
+            .word-count {
+              position: relative;
+              left: 95%;
+            }
+        `
+
+        this.attachShadow({mode: 'open'})
+
+        // Create (nested) elements
+        const wrapper = document.createElement('span')
+        wrapper.setAttribute('class', 'wrapper')
+
+        // Append element content
+        const Paragraph = wrapper.appendChild(document.createElement('p'))
+        Paragraph.append(this.textContent);
+
+        // Append our custom logic
+        const wordCount = wrapper.appendChild(document.createElement('small'))
+        wordCount.setAttribute('class', 'word-count')
+        wordCount.append(this.countWords(this.textContent) + ' words')
+
+        // Create some CSS to apply to the shadow DOM
+        const style = document.createElement('style')
+        style.textContent = styles
+
+        // attach the created elements to the shadow DOM
+        this.shadowRoot.append(style, wrapper)
+    }
+
+    countWords(str) {
+        const arr = str.split(' ')
+
+        return arr.filter(word => word !== '').length
+    }
+}
